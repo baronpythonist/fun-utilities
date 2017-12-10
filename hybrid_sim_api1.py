9370793e760d7e2133f423932229ab80046dd031
byronburks92 <byronburks92@gmail.com>
2017-12-10 15:00:56 -0500

    Add files via upload

--- hybrid_sim_api1.py
+++ hybrid_sim_api1.py
@@ -23,14 +23,21 @@ def allNotInvalid(data):
             return False
     else:
         return False
+    
+class ImmutableError(Exception):
+    """ This error is raised when an attempt is made to modify a constant signal """
+    def __init__(self, *args, **kwds):
+        super(self).__init__(*args, **kwds)
+        # that's all
 
 class Signal():
     """ A N-dimensional NumPy array of data, with a fixed shape and dtype """
-    def __init__(self, dShape, dtype1):
+    def __init__(self, dShape, dtype1, const=False):
         self.dShape = dShape
         self.dtype1 = dtype1
         self.data = np.empty(dShape, dtype=dtype1)
         self.initialized = False
+        self.isConstant = const
         self.connected_blocks = []
         
     def initData(self, data2):
@@ -45,7 +52,7 @@ class Signal():
                     self.initialized = True
                 else:
                     pass
-        except TypeError:
+        except (TypeError, ValueError):
             pass
         
     def addAsBlockInput(self, block1):
@@ -93,10 +100,13 @@ class Data_Monitor():
             updated = False
             if name in self.data:
                 signal1 = self.data[name]
-                try:
-                    updated = signal1.updateData(data2)
-                except TypeError:
-                    pass
+                if not signal1.isConstant:
+                    try:
+                        updated = signal1.updateData(data2)
+                    except (TypeError, ValueError):
+                        pass
+                else:
+                    raise ImmutableError
             else:
                 pass
             if updated:
@@ -112,6 +122,7 @@ class Sim_Block():
         self.nInputs = nInputs
         self.nOutputs = nOutputs
         self.initValues = {}
+        self.initialized = False
         self.function1 = simFunction1
         self.functData = functData
         self.props = props
@@ -120,6 +131,74 @@ class Sim_Block():
         self.inputNames = None
         self.outputNames = None
         
+    def init_outputs(self, outputNames, initSignals):
+        success = False
+        if not self.initialized:
+            if len(outputNames) == self.nOutputs:
+                outputInds = list(range(self.nOutputs))
+                for name, sigObject, n in zip(outputNames, initSignals, outputInds):
+                    if 'out' in self.ioPorts:
+                        outputs1 = self.ioPorts['out']
+                    else:
+                        outputs1 = {}
+                    if n in outputs1:
+                        if outputs1[n][1] is not None:
+                            pass
+                        else:
+                            outputs1[n] = (name, sigObject)
+                    else:
+                        outputs1[n] = (name, sigObject)
+                success = True
+            else:
+                pass
+        else:
+            pass
+        return success
+                
+        
     def connect(self, block2, linkPorts):
-        pass
+        success = False
+        if self.initialized:
+            for outN, inN in linkPorts:
+                if outN < self.nOutputs and inN < block2.nInputs:
+                    if 'out' in self.ioPorts:
+                        outputs1 = self.ioPorts['out']
+                        if outN in outputs1:
+                            (output_name, outObject) = outputs1[outN]
+                        else:
+                            break
+#                            output_name = 'output_{0:0>2d}'.format(outN)
+#                            outObject = None
+#                            outputs1[outN] = (output_name, outObject)
+#                            self.ioPorts['out'] = outputs1
+                    else:
+                        break
+#                        outputs1 = {}
+#                        for n in self.nOutputs:
+#                            output_name = 'output_{0:0>2d}'.format(n)
+#                            outObject = None
+#                            outputs1[n] = (output_name, outObject)
+#                        self.ioPorts['out'] = outputs1
+                    if 'in' in block2.ioPorts:
+                        inputs1 = block2.ioPorts['in']
+                    else:
+                        inputs1 = {}
+                    if inN in inputs1:
+                        pass
+                    else:
+                        input_name = output_name
+                        inObject = outObject
+                        inputs1[inN] = (input_name, inObject)
+                        block2.ioPorts['in'] = inputs1
+                    success = True
+                else:
+                    pass
+        else:
+            pass
+        return success
+    
+    
+                        
+                    
+                    
 
